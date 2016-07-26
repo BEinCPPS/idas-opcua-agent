5e14e08b102b3037fe41d851bc1966a75b806f31
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2016-07-26 09:37:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-node index.js -e "opc.tcp://UbuntuDesk:26543"  # Change with your endpoint -b
+node index.js -e "opc.tcp://UbuntuDesk:26543"  # Change with your endpoint 
+# -b 
+# "Browse" server address
+# -t <seconds> 
+# seconds before shut down Agent
